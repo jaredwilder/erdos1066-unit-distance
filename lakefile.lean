ca2b00726e751f3cbe64c55e20dbb05713d87432
jaredwilder <jared@jaredwilder.com>
2026-09-14 07:47:43 -0700

    build: pin FormalConjectures 4.27 dependency for #1066

---
+++ lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+
+open Lake DSL
+
+package erdos1066UnitDistance
+
+require formal_conjectures from git
+  "https://github.com/google-deepmind/formal-conjectures" @ "9f5ee773841921f460b4a26a3552f5eca4accaa0"
